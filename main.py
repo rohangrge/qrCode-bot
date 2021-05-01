5d9b9beac684d1f30c1b46c037e1708e18df29a0
rohangrge <rohangrge@gmail.com>
2021-05-01 20:23:58 +0530

    added error logging

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 import pyqrcode
 import png
 from pyqrcode import QRCode
-#from qrtools import QR
+# from qrtools import QR
 import qrtools
 from pyzbar import pyzbar
 import argparse
@@ -18,6 +18,7 @@ import cv2
 load_dotenv()
 
 client = discord.Client()
+logfile = open('logs.txt', 'a')
 
 
 @client.event
@@ -31,13 +32,11 @@ async def on_message(message):
         return
 
     imsg = message.content
+    logfile.write(imsg+'\n')
     if(imsg.startswith("$help")):
         async with message.channel.typing():
             await message.channel.send("$qrcode <link> to get the qrcode\n$rimg and attach image to read qr code")
-    if(imsg.startswith("$startgame")):
-        async with message.channel.typing():
-            # await message.channel.send(message.author.guild.id)
-            await message.reply(message.mentions)
+
     if(imsg.startswith("$qrcode")):
         async with message.channel.typing():
             link = imsg.split("$qrcode ", 1)[1]
@@ -46,21 +45,28 @@ async def on_message(message):
                 url.png('myqr.png', scale=6)
                 await message.reply(file=discord.File('myqr.png'))
                 os.remove('myqr.png')
+                logfile.write("succesfully created qr\n")
             except:
+                logfile.write("qr create unsuccesful \n")
                 await message.reply("sorry bot is unable to create qr")
     if(imsg.startswith("$rimg")):
         async with message.channel.typing():
             attachment = message.attachments
             await attachment[0].save(attachment[0].filename)
-            image = cv2.imread(attachment[0].filename)
-            barcodes = pyzbar.decode(image)
-            for barcode in barcodes:
-                barcodeData = barcode.data.decode("utf-8")
-            os.remove(attachment[0].filename)
-            await message.reply(barcodeData)
+            try:
+                image = cv2.imread(attachment[0].filename)
+                barcodes = pyzbar.decode(image)
+                for barcode in barcodes:
+                    barcodeData = barcode.data.decode("utf-8")
+                os.remove(attachment[0].filename)
+                await message.reply(barcodeData)
+                logfile.write("read qr succesful\n")
+            except:
+                logfile.write("read qr unsuccesful\n")
+                await message.reply("sorry,the bot is not optimised for this qr code,I am working on it")
 
 
 keep_alive()
 my_secret = os.getenv('token')
 print(my_secret)
-# client.run(my_secret)
+client.run(my_secret)
